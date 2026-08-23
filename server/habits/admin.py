bdc5945b94b0c954fb387511b4339b940f40bcf0
Muhammad Saqib <muhammadsaqib00353@gmail.com>
2026-08-24 03:14:14 +0500

    STREAK-6 Register models in Django admin

--- server/habits/admin.py
+++ server/habits/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Habit, HabitLog, Streak
 
-# Register your models here.
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'frequency', 'category', 'archived')
+
+    search_fields = ('name',)
+    
+@admin.register(HabitLog)
+class HabitLogAdmin(admin.ModelAdmin):
+    list_display = ('habit', 'date', 'completed', 'note')
+
+    search_fields = ('habit__name', 'date', 'note')
+
+@admin.register(Streak)
+class StreakAdmin(admin.ModelAdmin):
+    list_display = ('habit', 'current_streak', 'longest_streak', 'last_completed_date')
+
+    search_fields = ('habit__name',)
--- server/users/admin.py
+++ server/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'subscription_tier', 'subscription_status' ,'subscription_current_period_end')
+
+    search_fields = ('user__first_name', 'user__last_name', 'user__email', 'user__username')
